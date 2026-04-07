0e99e7ee3978a9ce6f3060db9557e6263da6155f
kakooza vianny <v9220933@gmail.com>
2026-04-07 10:24:17 +0300

    userstories

--- userstory.py
+++ userstory.py
@@ -2,3 +2,9 @@ user="student"
 action="software"
 goal="build apps"
 print(f"As a {user} i want to learn {action} so that i can {goal}")
+
+
+As a student, i want to create a weekly log  so that i can record what i did during the week .
+As a supervisor , i want to review a submitted weekly log and give a score so that the student gets feed back
+As  a supervisor , i want to view weekly logs and give  a score
+As an admin , i want to view all weekly logs  so that i can monitor the submission progress
