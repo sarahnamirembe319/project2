e159c55ee94551fe1898add0eb2dcd2b96000eb3
Bukenya Zirimu <programmingwithazirimu@gmail.com>
2026-04-09 14:18:58 +0300

    add settings.py file

---
+++ backend/settings.py
@@ -0,0 +1,16 @@
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+]
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ]
+}
