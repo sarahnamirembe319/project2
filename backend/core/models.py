3dd107fd9ed1d2361e0099597d8a276713ec1348
Bukenya Zirimu <programmingwithazirimu@gmail.com>
2026-04-12 21:22:20 +0300

    vaildationerror

--- backend/core/models.py
+++ backend/core/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import AbstractUser
-from django.db import models 
+from django.db import models   
+from django.core.exceptions import ValidationError
 
 class CustomUser(AbstractUser):
     ROLE-CHOICES = (
