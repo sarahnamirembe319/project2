82c09eebe39b2ea980ed23ea416565fe06d8dbc0
NAMIREMBE SARAH KAYIZZI <sarahnamirembe319@gmail.com>
2026-05-05 12:10:30 +0300

    Update settings, urls, serializers, views, and frontend

--- backend/iles_backend/urls.py
+++ backend/iles_backend/urls.py
@@ -3,9 +3,13 @@ from django.urls import path, include
 from django.http import HttpResponse
 from django.conf.urls.static import static
 from django.conf import settings
+from users_app.views import CustomTokenObtainPairView
+from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
     path('', lambda request: HttpResponse("API is working 🚀")),
+    path('api/auth/login/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/auth/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('admin/', admin.site.urls),
     path('api/auth/', include('issues_app.urls')),
     path('api/issues/', include('issues_app.urls')),
