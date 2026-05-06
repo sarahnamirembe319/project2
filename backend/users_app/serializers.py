47bc72e749e4a0175f147d1aa49f94fe9cbdbf37
NAMIREMBE SARAH KAYIZZI <sarahnamirembe319@gmail.com>
2026-05-06 18:37:47 +0300

    add user register serializer

--- backend/users_app/serializers.py
+++ backend/users_app/serializers.py
@@ -11,6 +11,21 @@ class UserSerializer(serializers.ModelSerializer):
         fields = ['id', 'username', 'email', 'first_name', 'last_name']
 
 
+class UserRegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, min_length=6)
+
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email', 'password', 'first_name', 'last_name']
+
+    def create(self, validated_data):
+        password = validated_data.pop('password')
+        user = User(**validated_data)
+        user.set_password(password)
+        user.save()
+        return user
+
+
 class IssueSerializer(serializers.ModelSerializer):
     class Meta:
         model = InternshipPlacement
