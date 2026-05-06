4cc48b7d05039ed6900a2747d2545230eded7505
NAMIREMBE SARAH KAYIZZI <sarahnamirembe319@gmail.com>
2026-05-06 23:37:37 +0300

    Fix password hashing

--- backend/users_app/serializers.py
+++ backend/users_app/serializers.py
@@ -5,26 +5,21 @@ from issues_app.models import InternshipPlacement
 User = get_user_model()
 
 
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ['id', 'username', 'email', 'first_name', 'last_name']
-
-
-class UserRegisterSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True, min_length=6)
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
 
     class Meta:
         model = User
-        fields = ['id', 'username', 'email', 'password', 'first_name', 'last_name']
+        fields = ["id", "username", "email", "password", "first_name", "last_name"]
 
     def create(self, validated_data):
-        password = validated_data.pop('password')
+        password = validated_data.pop("password")
+
         user = User(**validated_data)
         user.set_password(password)
         user.save()
-        return user
 
+        return user
 
 class InternshipPlacementSerializer(serializers.ModelSerializer):
     class Meta:
