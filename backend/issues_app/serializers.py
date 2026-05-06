1efdc93e96e62239d2a0e4de7a0f83677ab9db1b
NAMIREMBE SARAH KAYIZZI <sarahnamirembe319@gmail.com>
2026-05-06 19:19:27 +0300

    fix serializer import

--- backend/issues_app/serializers.py
+++ backend/issues_app/serializers.py
@@ -1,6 +1,13 @@
 from rest_framework import serializers
 from .models import InternshipPlacement, WeeklyLog, EvaluationCriteria, Evaluation
+from rest_framework import serializers
+from .models import InternshipPlacement
 
+
+class IssueSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = InternshipPlacement
+        fields = '__all__'
 
 class InternshipPlacementSerializer(serializers.ModelSerializer):
     class Meta:
