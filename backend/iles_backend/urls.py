bf31f91ccd226812684f76bc1f5bf6619dfe5cbc
NAMIREMBE SARAH KAYIZZI <sarahnamirembe319@gmail.com>
2026-05-06 23:06:27 +0300

    urls

--- backend/iles_backend/urls.py
+++ backend/iles_backend/urls.py
@@ -2,14 +2,17 @@ from django.contrib import admin
 from django.urls import path, include
 from django.http import JsonResponse
 
+
 def home(request):
     return JsonResponse({
         "message": "ILES backend is running",
         "status": "ok"
     })
 
+
 urlpatterns = [
     path("", home),
     path("admin/", admin.site.urls),
-    path("api/", include("issues_app.urls")),
+    path("api/auth/", include("users_app.urls")),
+    path("api/issues/", include("issues_app.urls")),
 ]
