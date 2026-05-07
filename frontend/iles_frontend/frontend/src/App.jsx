f49f975551e8aad7ee0d737d3a4d5595c159ce98
NAMIREMBE SARAH KAYIZZI <sarahnamirembe319@gmail.com>
2026-05-08 00:13:00 +0300

    Restore routes

--- frontend/iles_frontend/frontend/src/App.jsx
+++ frontend/iles_frontend/frontend/src/App.jsx
@@ -1,39 +1,17 @@
-// the main user inetrface
-import {BrowserRouter, Routes , Route} from "react-router-dom";
-import Login from "./pages/Login"
-import Dashboard from "./pages/Dashboard"
-import AuthProvider  from "./context/AuthContext";
-import ProtectedRoute from "./routes/ProtectedRoute";
-import SubmitLog from "./pages/SubmitLog";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-function App(){
-  return(
+import Login from "./pages/Login";
+import Dashboard from "./pages/Dashboard";
+
+function App() {
+  return (
     <BrowserRouter>
-      <AuthProvider>
-        <Routes>
-          {/* Public route - anyone can access */}
-          <Route path="/" element={<Login/>} />
-          {/* Protected route - only logged in users */}
-          <Route 
-            path="/dashboard" 
-            element={
-              <ProtectedRoute>
-
-                <Dashboard/>
-              </ProtectedRoute>
-            }
-          />  
-          <Route 
-            path="/submit-log" 
-            element={
-              <ProtectedRoute>
-                <SubmitLog/>
-              </ProtectedRoute>
-            }
-          />
-        </Routes>
-      </AuthProvider>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
     </BrowserRouter>
   );
 }
+
 export default App;
